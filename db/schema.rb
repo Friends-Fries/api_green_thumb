a9cb4aba7be2c84a2fa8b5fc7bb830a1a6466987
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2023-04-18 04:03:02 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_065029) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_070201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "jwt_blacklists", force: :cascade do |t|
+  create_table "jwt_denylists", force: :cascade do |t|
     t.string "jti"
     t.datetime "exp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["jti"], name: "index_jwt_blacklists_on_jti"
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
   create_table "users", force: :cascade do |t|
