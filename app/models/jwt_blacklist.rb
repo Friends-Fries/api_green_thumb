b6bc1744107f252de39c64d07e0f1b65d11827e6
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2023-04-18 03:54:59 -0300

    added comments to github gems

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -1,4 +1,6 @@
 class JwtBlacklist < ApplicationRecord
+  # https://github.com/waiting-for-dev/devise-jwt
+  
   include Devise::JWT::RevocationStrategies::Denylist
 
   self.table_name = 'jwt_blacklists'
