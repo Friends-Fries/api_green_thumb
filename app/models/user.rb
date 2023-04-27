6a6ead478cbbd949688fa726135287622ddf6dd5
Atila Lima <atilamanager@gmail.com>
2023-04-27 15:44:20 -0300

    Adding devise trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # https://github.com/heartcombo/devise
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :trackable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
