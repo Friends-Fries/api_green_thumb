4eb572b235181feecc433790141b73104c30d4ea
Atila Lima <atilamanager@gmail.com>
2023-05-04 14:53:49 -0300

    adding null: false for migration files items

--- db/migrate/20230427173240_create_cities.rb
+++ db/migrate/20230427173240_create_cities.rb
@@ -1,8 +1,8 @@
 class CreateCities < ActiveRecord::Migration[7.0]
   def change
     create_table :cities do |t|
-      t.string :name
-      t.string :uf
+      t.string :name, null: false
+      t.string :uf, null: false
       t.references :country, null: false, foreign_key: true
 
       t.timestamps
