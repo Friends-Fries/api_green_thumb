879c1cf3cde48923ae72c2c332d0e32dd642979b
victor <victor.morelli.007@gmail.com>
2023-05-18 20:51:08 -0300

    added tests, validations and comments to all models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not be valid without" do
+    user_without_email = User.new
+
+    assert_not user_without_email.valid?
+
+    assert user_without_email.errors.full_messages, "Email can't be blank"
+  end
 end
