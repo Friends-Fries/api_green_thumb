53b37ab4ae136ec37eaa2f00de5e2e35de2c2a91
victor <victor.morelli.007@gmail.com>
2023-05-18 21:14:51 -0300

    added more tests to users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,16 @@ class UserTest < ActiveSupport::TestCase
 
     assert user_without_email.errors.full_messages, "Email can't be blank"
   end
+
+  test "new user should be volunteer" do
+    new_user = User.new
+
+    assert new_user.role, "volunteer"
+  end
+
+  test "new user should not be an admin" do
+    new_user = User.new
+
+    assert_not new_user.admin?, true
+  end
 end
