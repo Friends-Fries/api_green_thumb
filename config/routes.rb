cf3ea4df5c252d026b64a32a71766c4bbd2b3842
victor <victor.morelli.007@gmail.com>
2023-08-23 21:29:56 -0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :city
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
