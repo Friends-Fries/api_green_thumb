7e1826ff1cc35cc1b3675e79a87fad54789b0838
victor <victor.morelli.007@gmail.com>
2023-08-27 22:23:59 -0300

    sessions and controllers overriding devise methods to be an API

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,20 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
-end
 
-# Requests from the client
-# It will be mostly POST requests to create new users in our APP
+  private
+
+  # Overriding from devise
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed up successfully." },
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes],
+      }
+    else
+      render json: {
+               status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" },
+             }, status: :unprocessable_entity
+    end
+  end
+end
